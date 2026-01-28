030c7132e60614432da5e06a7423f6fa0be04d08
pixiemays <kondakov2007.andrey@gmail.com>
2026-01-29 00:46:33 +0700

    meow murrr full lab3

--- app/src/main/java/com/pixiemays/newmeowapplab3/MainActivity.kt
+++ app/src/main/java/com/pixiemays/newmeowapplab3/MainActivity.kt
@@ -1,48 +1,51 @@
 package com.pixiemays.newmeowapplab3
 
-import android.app.Fragment
-import android.app.FragmentManager
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        loadFragment(FirstFragment())
-
-        val btn1: Button = findViewById(R.id.btn_fragment1)
-        val btn2: Button = findViewById(R.id.btn_fragment2)
-        val btn3: Button = findViewById(R.id.btn_fragment3)
-
-        btn1.setOnClickListener {
+        if (savedInstanceState == null) {
             loadFragment(FirstFragment())
         }
 
-        btn2.setOnClickListener {
-            loadFragment(SecondFragment())
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_fragment1 -> {
+                    loadFragment(FirstFragment())
+                    true
+                }
+                R.id.nav_fragment2 -> {
+                    loadFragment(SecondFragment())
+                    true
+                }
+                R.id.nav_fragment3 -> {
+                    loadFragment(ThirdFragment())
+                    true
+                }
+                else -> false
+            }
         }
 
-        btn3.setOnClickListener {
-            loadFragment(ThirdFragment())
-        }
+        bottomNav.selectedItemId = R.id.nav_fragment1
     }
 
-    fun loadFragment(fragment: androidx.fragment.app.Fragment) {
-        val fm: androidx.fragment.app.FragmentManager = supportFragmentManager
+    private fun loadFragment(fragment: androidx.fragment.app.Fragment) {
+        val fm = supportFragmentManager
         val ft: FragmentTransaction = fm.beginTransaction()
         ft.replace(R.id.fragment_container, fragment)
         ft.commit()
